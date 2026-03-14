1c49cca60a2956bd7e871265571c94aa5593f180
unknown <2809ruchika@gmail.com>
2026-03-14 11:46:37 +0530

    fix: clean config, no URI manipulation

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI") + "?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true"
     SECRET_KEY = os.getenv("SECRET_KEY", "a32a219889454456c86cf413e2a41f06c39652135c7c66c81a4f8ca89778bae4")
+    MONGO_URI = os.getenv("MONGO_URI")
     DB_NAME = os.getenv("DB_NAME", "sentinelvoteai_production")
     FACE_TOLERANCE = float(os.getenv("FACE_TOLERANCE", "0.6"))
     BEHAVIOR_THRESHOLD = float(os.getenv("BEHAVIOR_THRESHOLD", "0.3"))
