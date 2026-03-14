f259fb11efb8db84e63ce7978cae466d6e3db605
unknown <2809ruchika@gmail.com>
2026-03-14 11:36:38 +0530

    fix: add TLS fix for MongoDB Atlas SSL handshake

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/sentivoteai")
+    MONGO_URI = os.getenv("MONGO_URI") + "?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true"
     SECRET_KEY = os.getenv("SECRET_KEY", "a32a219889454456c86cf413e2a41f06c39652135c7c66c81a4f8ca89778bae4")
     DB_NAME = os.getenv("DB_NAME", "sentinelvoteai_production")
     FACE_TOLERANCE = float(os.getenv("FACE_TOLERANCE", "0.6"))
