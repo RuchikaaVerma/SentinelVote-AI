d679b63e6e05df411906d07b270ad12dec59dffc
unknown <2809ruchika@gmail.com>
2026-03-14 11:30:01 +0530

    fix: explicitly set database name in PyMongo init

--- database/db.py
+++ database/db.py
@@ -4,3 +4,9 @@ mongo = PyMongo()
 
 def init_db(app):
     mongo.init_app(app)
+    
+    # Force database name from env
+    import os
+    db_name = os.getenv("DB_NAME", "sentinelvoteai_production")
+    mongo.db = mongo.cx[db_name]
+    
